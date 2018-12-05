5aa55c9ac1fb076ebbeee72279bdaf6eb71bc231
Thomas Sandberg (DevTactix) <mr.thomas.sandberg@gmail.com>
2018-12-05 22:25:02 +0100

    Create basic fizzbuzz

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 (function fizzbuzz() {
+    'use strict';
 
+    const START = 0;
+    const LENGTH = 100;
+
+    const isZero = x => x === 0;
+    const isDivisibleBy3 = x => x % 3 === 0;
+    const isDivisibleBy5 = x => x % 5 === 0;
+    const isDivisibleBy3And5 = x => isDivisibleBy3(x) && isDivisibleBy5(x);
+
+    for (let i = START; i <= (LENGTH - 1); i ++) {
+        let x = isZero(i) ?
+            i.toString()
+        : isDivisibleBy3And5(i) ?
+            "FizzBuzz"
+        : isDivisibleBy3(i) ?
+            "Fizz"
+        : isDivisibleBy5(i) ?
+            "Buzz"
+        :   i.toString();
+
+        console.log(`${i.toString()}: ${x}`);
+    }
 })();
